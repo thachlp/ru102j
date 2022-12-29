b7f02de810a0227701c604eab5eeabcb0e6d7c13
Thach Le <lephuocthach@gmail.com>
2022-12-29 16:59:20 +0700

    Check null

--- src/main/java/com/redislabs/university/RU102J/dao/SiteDaoRedisImpl.java
+++ src/main/java/com/redislabs/university/RU102J/dao/SiteDaoRedisImpl.java
@@ -46,7 +46,7 @@ public class SiteDaoRedisImpl implements SiteDao {
             Set<Site> sites = new HashSet<>(keys.size());
             for (String key : keys) {
                 Map<String, String> fields = jedis.hgetAll(key);
-                if (!fields.isEmpty()) {
+                if (fields != null && !fields.isEmpty()) {
                     sites.add(new Site(fields));
                 }
             }
